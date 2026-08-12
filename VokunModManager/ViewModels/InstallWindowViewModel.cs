ecdd901c9e781c3a09b77a8acfee55bbc8171f99
E1ecTro5 <121376336+E1ecTro5@users.noreply.github.com>
2026-08-12 06:04:34 +0400

    fix: install window fixed bugs while selecting plugins.

--- VokunModManager/ViewModels/InstallWindowViewModel.cs
+++ VokunModManager/ViewModels/InstallWindowViewModel.cs
@@ -11,13 +11,13 @@ namespace VokunModManager.ViewModels;
 public partial class InstallWindowViewModel : ViewModelBase
 {
     private readonly TaskCompletionSource<List<PluginOption?>>? _tcs;
-    private readonly string _type;
+    [ObservableProperty] private string _type;
 
     [ObservableProperty] private string _groupName;
     [ObservableProperty] private List<PluginOption> _options;
 
-    public bool IsSingleSelect => _type == "SelectExactlyOne" || _type == "SelectAtMostOne";
-    public bool IsMultiSelect => _type == "SelectAny";
+    public bool IsSingleSelect => _type is "SelectExactlyOne" or "SelectAtMostOne";
+    public bool IsMultiSelect => _type is "SelectAny" or "SelectAll" or "SelectAtLeastOne";
     
     public ICommand SelectCommand { get; }
 
@@ -29,16 +29,22 @@ public partial class InstallWindowViewModel : ViewModelBase
         _type = group.Type;
         Options = group.Plugins.ToList();
 
-        foreach (var option in _options)
-        {
-            option.Description = option.Description.Trim();
-        }
+        foreach (var option in _options) option.Description = option.Description.Trim();
 
+        // select by default specifically to this type
+        if (_type == "SelectExactlyOne" && !Options.Any(x => x.IsSelected) && Options.Count > 0) Options[0].IsSelected = true;
+        
         SelectCommand = new RelayCommand(SelectButton);
     }
     
     private void SelectButton()
     {
+        var selectedCount = Options.Count(x => x.IsSelected);
+        
+        if (_type == "SelectExactlyOne" && selectedCount != 1) return;
+        if (_type == "SelectAtLeastOne" && selectedCount < 1) return;
+        if (_type == "SelectAtMostOne" && selectedCount > 1) return;
+        
         // get all the selected files
         var selectedOptions = Options.Where(x => x.IsSelected).Cast<PluginOption?>().ToList();
         
